09a19429607ea596a288e4135c9e31c6498333fb
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-03-10 16:47:37 +0500

    done

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ userBioData[2] = prompt("Enter Your Surname");
 userBioData[3] = prompt("Enter Your Phone Number");
 userBioData[4] = prompt("Enter Your Email Address");
 userBioData[5] = prompt("Enter Your Qualification");
+userBioData.splice(4 , 5 );
 console.log(userBioData)
 userBioData.push("hello , bye , goodbye ");
 userBioData.unshift("hi , do good deeds in ramdan");
-// userBioData.pop()
-// userBioData.pop()
-// userBioData.pop()
-// userBioData.shift()
-var copy = userBioData.slice(2 , 5);
-console.log(userBioData)
+userBioData.pop()
+userBioData.pop()
+userBioData.pop()
+userBioData.shift()
+var copy = userBioData.slice(0 , 3);
+console.log(copy)
